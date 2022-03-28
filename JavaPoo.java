a5755da962a2b62279a3dbdcef989478bd483336
Carlos Antônio <95361309+carlosantonio212@users.noreply.github.com>
2022-03-28 12:18:51 +0000

    java

---
+++ JavaPoo.java
@@ -0,0 +1,31 @@
+package programajava;
+
+public class JavaPoo {
+
+    public static void main(String[] args) {
+   	    
+   		Double x= 0d;
+      	 
+       	Float y= 10f;
+      	 
+       	Boolean z=false;
+      	 
+       	System.out.println(x.MAX_VALUE);
+      	 
+       	System.out.println(z.logicalXor(false,true));
+      	 
+       	System.out.println(Integer.toBinaryString(80));
+      	 
+       	System.out.println(Integer.toHexString(80));
+      	 
+       	System.out.println(Integer.toOctalString(80));
+      	 
+       	System.out.println(y.max(10f, 80f));
+      	 
+       	System.out.println(y.min (10f, 80f));
+      	 
+
+   	 
+    }
+
+}
